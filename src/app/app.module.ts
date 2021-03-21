e459b5d7bcb148d65a8eb4700efcca569baffc44
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-03-21 17:08:44 +0530

    # adding auto generated dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { 
+  MatToolbarModule, 
+  MatSidenavModule, 
+  MatIconModule, 
+  MatListModule, 
+  MatGridListModule, 
+  MatCardModule, 
+  MatMenuModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MatButtonModule,
     LayoutModule,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
